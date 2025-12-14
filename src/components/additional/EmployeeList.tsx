d8bc6fc8fd0b5d4ceb8b3f09da620b5e7debbec6
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-14 23:01:16 +0400

    since it The API does not give me the permission to remove concrete user CORS ERROR I can not remove it naturally, but I will do it locally so visually it will be displayed and than build also hook form with zod and @hookform/resolver. goal was only one to understand and use things that I just have learned or relearned

--- src/components/additional/EmployeeList.tsx
+++ src/components/additional/EmployeeList.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 interface UserList {
@@ -35,6 +35,15 @@ const EmployeeList = () => {
       return response.data as UserList[];
     },
   });
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: async (id: number) => {
+      return axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["employeeList"] });
+    },
+  });
   if (isLoading)
     return (
       <div className="flex justify-center align-center text-white font-bold">
@@ -50,36 +59,46 @@ const EmployeeList = () => {
   const tableHeader = ["Name", "Email", "Company"];
 
   return (
-    <div className="flex flex-col justify-center items-center h-[30rem] bg-white rounded-3xl shadow-lg overflow-y-auto p-6">
-      <table className="w-full border-separate border-spacing-y-2 border-l-2 border-r-2 border-gray-50">
-        <thead className=" bg-gray-50">
-          <tr>
-            {tableHeader.map((item) => (
-              <th
-                className="text-left uppercase text-xs tracking-wider text-gray-600 px-4 py-3 select-none"
-                key={item}
-              >
-                {item}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="transition-colors duration-200">
-          {data?.map((item: UserList) => (
-            <tr
-              className="odd:bg-white even:bg-gray-50 hover:bg-blue-100 transition-colors duration-200"
-              key={item.id}
-            >
-              <td className="font-medium text-gray-900"> {item.name} </td>
-              <td className="text-gray-600"> {item.email} </td>
-              <td className="inline-flex items-center rounded-full bg-gray-100 px-3 py-1 text-sm text-gray-700">
-                {" "}
-                {item.company.name}{" "}
-              </td>
+    <div className="flex justify-center items-center flex-col block">
+      <div className="flex flex-col justify-center items-center h-[30rem] bg-white rounded-3xl shadow-lg overflow-y-auto p-6 w-full">
+        <table className="w-full border-separate border-spacing-y-2 border-l-2 border-r-2 border-gray-50">
+          <thead className=" bg-gray-50">
+            <tr>
+              {tableHeader.map((item) => (
+                <th
+                  className="text-left uppercase text-xs tracking-wider text-gray-600 px-4 py-3 select-none"
+                  key={item}
+                >
+                  {item}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="transition-colors duration-200">
+            {data?.map((item: UserList) => (
+              <tr
+                className="odd:bg-white even:bg-gray-50 hover:bg-blue-100 transition-colors duration-200"
+                key={item.id}
+              >
+                <td className="font-medium text-gray-900"> {item.name} </td>
+                <td className="text-gray-600"> {item.email} </td>
+                <td className="inline-flex items-center rounded-full bg-gray-100 px-3 py-1 text-sm text-gray-700">
+                  {" "}
+                  {item.company.name}{" "}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <button
+        className="rounded-lg bg-blue-500 w-72 h-10 mt-10 "
+        type="button"
+        disabled={mutation.isPending}
+        onClick={() => mutation.mutate(2)}
+      >
+        {mutation.isPending ? "Deleting..." : "Delete"}
+      </button>
     </div>
   );
 };
