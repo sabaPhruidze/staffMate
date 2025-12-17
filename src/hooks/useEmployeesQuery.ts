898eb24cbff76792745236cafdde1a85d697193d
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-17 21:58:01 +0400

    what I am trying is to make get, post and delete in different hooks so I will use individually where I need and whichever I need

---
+++ src/hooks/useEmployeesQuery.ts
@@ -0,0 +1,13 @@
+import {useQuery} from "@tanstack/react-query"
+import axios from 'axios';
+import type { UserList,UseEmployeesArg } from "../types/employee";
+
+export const useEmployeesQuery = ({key , api}: UseEmployeesArg) => {
+ return useQuery({
+    queryKey:key,
+    queryFn: async() => {
+        const {data} = await axios.get(api);
+         return 
+    }
+ })
+}
